62f96f89b74e6836b3ef070d3d59c021e6d3fc80
unknown <baluplay15@gmail.com>
2024-08-06 12:07:56 +0500

    husan lox

---
+++ src/component/navbar/index.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@material-tailwind/react";
+
+export default function Navbar(){
+    return <div className="navbar">
+        <div className="logo">logo
+            
+        </div>
+        <div className="btns">
+            <Button>Get Donation</Button>
+            <Button variant="outlined">Report</Button>
+        </div>
+
+
+
+    </div>
+}
